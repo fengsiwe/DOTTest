3e9f2dd83d6ca00123a137eae09b5e7aa31ef5df
Gary <gary@MacBook-Pro-2.local>
2024-07-24 20:49:54 +1200

    feat: set up sqlite db and create outlets transaction database

--- solution.py
+++ solution.py
@@ -3,6 +3,21 @@ import csv
 
 def setup_database(db_name='outlets_transactions.db'):
     """Create the transactions table using SQLite database"""
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    #Create outlets transaction table
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS transactions (
+        shop_id INTEGER,
+        date TEXT,
+        n_trans INTEGER
+    );
+    """
+
+    cursor.execute(create_table_query)
+    conn.commit()
+    conn.close()
 
 
 def load_data(csv_file_path, db_name='outlets_transactions.db'):
@@ -11,3 +26,6 @@ def load_data(csv_file_path, db_name='outlets_transactions.db'):
 
 def create_view(db_name='outlets_transactions.db'):
     """Create a view which customer required"""
+
+#Execute here
+setup_database()
